19547ab6048f3e06aa3cf22bff2c104dd630b85a
Tiago Alves Macambira <tmacam@burocrata.org>
2007-05-28 20:19:10 +0000

    Optimizing word iteration

---
+++ src/TokenIterator.h
@@ -0,0 +1,86 @@
+//: C07:TokenIterator.h
+#ifndef TOKENITERATOR_H
+#define TOKENITERATOR_H
+#include <algorithm>
+#include <cctype>
+#include <functional>
+#include <iterator>
+#include <string>
+
+struct Isalpha : std::unary_function<char, bool> {
+	bool operator()(char c) {
+		return not (std::ispunct(c) || std::isspace(c));
+	}
+};
+
+class Delimiters : std::unary_function<char, bool> {
+	std::string exclude;
+public:
+	Delimiters() {}
+	Delimiters(const std::string& excl)
+		: exclude(excl) {}
+	bool operator()(char c) {
+		return exclude.find(c) == std::string::npos;
+	}
+};
+
+template <class InputIter, class Pred = Isalpha>
+class TokenIterator : public std::iterator<
+		      std::input_iterator_tag,std::string, std::ptrdiff_t>
+{
+	InputIter first;
+	InputIter last;
+	std::string word;
+	Pred predicate;
+public:
+	TokenIterator(InputIter begin, InputIter end, Pred pred = Pred())
+	: first(begin), last(end), predicate(pred)
+	{
+		++*this;
+	}
+
+	TokenIterator() {} // End sentinel
+
+	// Prefix increment:
+	TokenIterator& operator++()
+	{
+		word.clear();
+		first = std::find_if(first, last, predicate);
+		while (first != last && predicate(*first)){
+			word += *first;
+			++first;
+		}
+		return *this;
+	}
+
+	// Postfix increment
+	class Proxy {
+		std::string word;
+		public:
+		Proxy(const std::string& w) : word(w) {}
+		std::string operator*() { return word; }
+	};
+	Proxy operator++(int) {
+		Proxy d(word);
+		++*this;
+		return d;
+	}
+	// Produce the actual value:
+	std::string operator*() const { return word; }
+	std::string* operator->() const
+	{
+		return &(operator*());
+	}
+
+	// Compare iterators:
+	bool operator==(const TokenIterator&)
+	{
+		return word.size() == 0 && first == last;
+	}
+
+	bool operator!=(const TokenIterator& rv)
+	{
+		return !(*this == rv);
+	}
+};
+#endif // TOKENITERATOR_H ///:~
--- src/indexer.cpp
+++ src/indexer.cpp
@@ -3,10 +3,25 @@
 #include "zfilebuf.h"
 #include "htmlparser.h"
 #include "strmisc.h"
+#include "entityparser.h"
+
+#include "TokenIterator.h"
 
 #include <iterator>
 #include <algorithm>
 #include <list>
+#include <ext/hash_map>
+#include <tr1/functional>
+
+
+
+/***********************************************************************
+			     Typedefs and constants
+ ***********************************************************************/
+
+typedef __gnu_cxx::hash_map < std::string, int,
+                        std::tr1::hash<std::string> > StrIntMap;
+
 
 /***********************************************************************
 			      HTMLContentRetriever
@@ -121,6 +136,11 @@ filebuf HTMLContentRetriever::pull()
 			      HTMLContentIterator
  ***********************************************************************/
 
+/**Allows one to iterate over all text nodes from a HTML file.
+ *
+ * Entities are converted in the process.
+ *
+ */
 class HTMLContentIterator
 : public std::iterator<std::input_iterator_tag, std::string >
 {
@@ -146,7 +166,7 @@ public:
 	//! Prefix increment operator
 	HTMLContentIterator& operator++()
 	{
-		text = content.pull().str();
+		text = parseHTMLText( content.pull() );
 
 		return *this;
 	}
@@ -188,24 +208,203 @@ public:
 };
 
 
+/***********************************************************************
+		       HTMLContentWideCharStreamIterator
+ ***********************************************************************/
+
+/**Gives a ilusion of a stream of initerrupted text content.
+ *
+ * Gives a ilusion that a HTML file has a single text node.
+ * A text node is made by text inside and between tags.
+ *
+ * @deprecated Give poor reference locality and thus poor performance.
+ */
+class HTMLContentWideCharStreamIterator
+: public std::iterator<std::input_iterator_tag, char >
+{
+private:
+	//!Data do be parsed (enveloped in a parser)
+	HTMLContentRetriever content;
+
+	//! Last "amount" of text retrieved from the contents
+	std::string text;
+	std::string::size_type text_pos;
+
+	//! Was the separator between two consecutive text contents issued?
+	bool put_sep;
+
+	//! The last character read - if any
+	value_type c;
+public:
+
+	/**Default constructor */
+	HTMLContentWideCharStreamIterator(const filebuf cont)
+	: content(cont), text(), text_pos(0), put_sep(false)
+	{
+		++*this;
+	}
+
+	/**Default constructor.
+	 *
+	 * Use to build end() sentinels.
+	 */
+	HTMLContentWideCharStreamIterator()
+	: content(filebuf()), text(), text_pos(0), put_sep(false)
+	{}
+
+	/**Copy Constructor.
+	 * 
+	 * This copy constructor has some issues but seems to be better
+	 * then the C++ automatically generated one.
+	 *
+	 * Its biggest flawn is that text_pos allways rewinds to text.begin()*/
+	HTMLContentWideCharStreamIterator(
+		const HTMLContentWideCharStreamIterator& original)
+	: content(original.content), text(original.text,original.text_pos),
+	  text_pos(0), put_sep(original.put_sep)
+	{}
+	
+
+	//! Prefix increment operator
+	inline HTMLContentWideCharStreamIterator& operator++()
+	{		
+		// Is there data left in the buffer?
+		if (text_pos != text.size()){
+			++text_pos;
+		} else {
+			if ( !content.eof()) {
+				if (! put_sep ) {
+					text = " ";
+					text_pos = 0;
+					put_sep = true;
+				} else {
+					// Separator already issued
+					// Get new content
+					filebuf cont;
+					while(!content.eof() &&
+					      (cont = content.pull()).eof() ){ }
+					text = parseHTMLText( cont );
+					text_pos = 0;
+					put_sep = false;
+				}
+			}
+		}
+
+		return *this;
+	}
+
+	//!Postfix increment operator helper class
+	class Proxy {
+		value_type tmp_val;
+	public:
+		Proxy(const value_type& val): tmp_val(val) {}
+		value_type operator*() {return tmp_val;}
+	};
+
+	//!Postfix increment operator
+	Proxy operator++(int)
+	{
+		Proxy d( *(*this) );
+		++*this;
+		return d;
+	}
+
+	value_type operator*() const { return text[text_pos]; }
+	//inline pointer operator->() {return text_pos; }
+
+	/**Comparison operator.
+	 *
+	 * Actually, it only verifies if this iterator has reached the
+	 * end of the data
+	 */
+	inline bool operator==(const HTMLContentWideCharStreamIterator& other)
+	{
+		return content.eof() && (text_pos == text.size());
+	}
+
+	inline bool operator!=(const HTMLContentWideCharStreamIterator& other)
+	{
+		return !operator==(other);
+	}
+
+};
 
 
 /***********************************************************************
 				      MAIN
  ***********************************************************************/
 
+template<class T>
+void imprime_conteudo(T is, T ie)
+{
+	std::ostream_iterator< typename T::value_type> os(std::cout,"");
+//        for(; is != ie; ++is, ++os) {
+//                *os = *is;
+//        }
+	std::copy(is, ie , os);
+}
+
+void assert_equal(HTMLContentWideCharStreamIterator i, HTMLContentWideCharStreamIterator& e, HTMLContentWideCharStreamIterator end)
+{
+	while(i != end){
+		assert(*i == *e);
+		++i; ++e;
+	}
+}
+
+/**Blah.
+ *
+ * wfreq is cleared at every function call.
+ */
+void getWordFrequency(filebuf f, StrIntMap& wfreq)
+{
+	Isalpha isalpha;
+
+	HTMLContentIterator ci(f), ce;
+	std::string word;
+
+	// Clear word frequency
+	wfreq.clear();
+
+	// For each text node (text inside and between tags) content
+	for(; ci != ce; ++ci){
+		std::string text_node = *ci;
+		std::string::const_iterator i(text_node.begin()),
+				      end(text_node.end());
+		// Get all words from this text node 
+		while(i != text_node.end()){
+			// new word
+			word.clear();
+			// Find word start
+			i = std::find_if(i,end,isalpha);
+			// Go until word end
+			while(i != end && isalpha(*i)){
+				word += *i;
+				++i;
+			}
+			// Grabbed a full word. 
+			wfreq[word] = wfreq[word] + 1;
+		}
+
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	for(int i = 1; i < argc; ++i) {
-		try{
+//                try{
 			AutoFilebuf dec(decompres(argv[i]));
 			filebuf f = dec.getFilebuf();
-			HTMLContentIterator it(f), endit;
-			std::ostream_iterator<std::string> os(std::cout, " ");
-			std::copy(it, endit, os);
-		} catch(...) {
+			std::ostream_iterator<std::string> os(std::cout," ");
+//                        std::copy(it, endit, os);
+			HTMLContentWideCharStreamIterator is(f), ends;
+			TokenIterator<HTMLContentWideCharStreamIterator>
+				wordIter(is, ends), end;
+			std::copy(wordIter, end, os);
+//                } catch(...) {
+//                        throw;
 			// pass
-		}
+//                }
 	}
 
 	std::cout << "Done." << std::endl;
