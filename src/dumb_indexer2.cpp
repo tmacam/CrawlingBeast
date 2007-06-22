8f95982401336a5a2c281c9986651f3f746ce111
Tiago Alves Macambira <tmacam@burocrata.org>
2007-06-22 01:43:33 +0000

    * mkstore: 	- Moving typedefs to .hpp  * dumb_indeer2: 	- Created based on un-commited dumb_indexer. Uses the document store 	  instead of the crawled files.  * Makefile: 	- modified to hanle dumb_indexer2

---
+++ src/dumb_indexer2.cpp
@@ -0,0 +1,92 @@
+// vim:syn=cpp.doxygen:autoindent:smartindent:fileencoding=utf-8:fo+=tcroq:
+
+#include "indexer.h"
+#include "zfilebuf.h"
+#include "strmisc.h"
+#include "indexerutils.hpp"
+#include "zfilebuf.h"
+#include "crawlerutils.hpp"
+#include "isamutils.hpp"
+#include "mkstore.hpp"
+
+#include <fstream>
+#include <sstream>
+
+
+struct DumbIndexerVisitor {
+	// Statistics
+	docid_t d_count;
+	uint64_t byte_count;
+	uint64_t last_byte_count;
+	time_t last_broadcast;
+	time_t time_started;
+
+	DumbIndexerVisitor()
+	: d_count(0), byte_count(0), last_byte_count(0),
+	  last_broadcast(time(NULL)), time_started(time(NULL))
+	{}
+
+	void operator()(uint32_t count, const store_hdr_entry_t* hdr,
+			filebuf store_data)
+	{
+		uint32_t* docid = (uint32_t*) store_data.read(sizeof(uint32_t));
+		uint32_t* len = (uint32_t*) store_data.read(sizeof(uint32_t));
+		assert(*docid == hdr->docid);
+
+
+		// read document ( no decompressing)
+		filebuf f(store_data.readf(*len));
+
+		// Statistics and prefetching
+		++d_count;
+		byte_count += f.len();
+		if (d_count  % 1000 == 0) {
+			print_stats();
+		}
+	}
+
+	void print_stats()
+	{
+		time_t now = time(NULL);
+
+		if (now == last_broadcast) return; // Avoid FPErr
+
+		std::cout << "In stats" << d_count << std::endl;
+
+		uint64_t byte_amount = byte_count - last_byte_count;
+		std::cout << "# docs: " << d_count << " bytes: " <<
+			byte_amount << " / " << byte_count << " bps: "<<
+			byte_amount/(now - last_broadcast) << 
+			" elapsed " << now - time_started << std::endl;
+
+		last_broadcast = now;
+		last_byte_count = byte_count;
+	}
+};
+
+
+
+int main(int argc, char* argv[])
+{
+	const char* store_dir;
+
+	if(argc < 2) {
+		std::cerr << "wrong number of arguments" << std::endl;
+		std::cerr << "dumb_indexer2 store_dir" << std::endl;
+		exit(1);
+	}
+
+	store_dir = argv[1];
+
+	DumbIndexerVisitor visitor;
+
+	VisitIndexedStore<store_hdr_entry_t>(store_dir, "store", visitor);
+
+	sleep(1);
+	visitor.print_stats();
+
+	exit(0);
+}
+
+
+//EOF
---
+++ src/mkstore.hpp
@@ -0,0 +1,68 @@
+// vim:syn=cpp.doxygen:autoindent:smartindent:fileencoding=utf-8:fo+=tcroq:
+#ifndef __MKSTORE_H__
+#define __MKSTORE_H__
+/**@file mkstore.hpp
+ * @brief Build a document store from set of crawled and downloaded docids.
+ *
+ * The document store consists of two parts:
+ *
+ * - An index file, store.idx
+ *
+ *   Which stores a fixed width index. Each entry in this index is a
+ *   @c store_hdr_entry_t
+ *
+ * - A group of store data files (store.data_xxxx).
+ *
+ *   Each store data file consists of a sequence of
+ *
+ *   - docid
+ *   - length of the document contents compressed
+ *   - document contents
+ *
+ */
+
+#include "common.h"
+
+/***********************************************************************
+				    Typedefs
+ ***********************************************************************/
+
+/**Structure for document store index's entries.
+ */
+struct store_hdr_entry_t {
+	uint32_t docid;	//!< DocId of the document
+	uint16_t fileno; /**< Number of the store data file holding the contents
+			 *   of the document with id @p docid
+			 */
+	uint32_t pos;	/**< Position of the document in the document store 
+			 *   data file with number @c fileno
+			 */
+
+
+	store_hdr_entry_t(uint32_t id = 0, uint8_t n=0, uint32_t position=0)
+	: docid(id), fileno(n),  pos(position)
+	{}
+} __attribute__((packed));
+
+
+struct store_data_entry_t {
+	uint32_t docid;	//!< DocId of the document
+	uint32_t len;	//!< Length of the document
+
+	store_data_entry_t(uint32_t _id=0, uint32_t _len=0)
+	: docid(_id), len(_len)
+	{}
+
+} __attribute__((packed));
+
+
+
+
+
+
+#endif // __MKSTORE_H__
+
+//EOF
+
+
+
