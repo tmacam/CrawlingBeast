0c4aecf3914ce5b7965879b5b3003a472ba513c1
Tiago Alves Macambira <tmacam@burocrata.org>
2007-06-25 19:57:27 +0000

    * merger:  	- fromString template moved to strmisc.h some time ago...

--- src/merger.cpp
+++ src/merger.cpp
@@ -1,20 +1,10 @@
 // vim:syn=cpp.doxygen:autoindent:smartindent:fileencoding=utf-8:fo+=tcroq:
 #include "slidingreader.hpp"
 #include "mergerutils.hpp"
+#include "strmisc.h"
 
 #include <iostream>
 #include <queue>
-
-template <class T>
-T fromString(std::string s)
-{
-	T val;
-	std::istringstream reader(s);
-	reader >> val;
-
-	return val;
-
-}
 
 void show_usage()
 {
